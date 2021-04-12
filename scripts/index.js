66156b34fb8b89fb80c435f560fe16a83ace0a5d
Buckley212 <rob36611@gmail.com>
2021-04-12 14:51:11 -0400

    added player rotation and aiming

--- scripts/index.js
+++ scripts/index.js
@@ -5,45 +5,64 @@ let ctx = canvas.getContext('2d');
 const backgroundImage = new Image();
 backgroundImage.src = './resources/assets/gameMap.PNG'
 
-const playerImage = new Image();
-playerImage.src = './resources/assets/survivor-idle_shotgun_0.png'
+const player_image = new Image();
+player_image.src = './resources/assets/survivor-idle_shotgun_0.png'
 
+let gMouseX = 0;
+let gMouseY = 0;
+let gShipAngleInRads = 0;
+let centerOfShipX = 0;
+let centerOfShipY = 0;
+let actualMouseX = 0;
+let actualMouseY = 0;
 
+document.addEventListener("mousemove", (e) => {
+    gMouseX = e.clientX;
+    gMouseY = e.clientY;
+  });
 
 class Player {
-    constructor(ctx, x, y) {
-      this.x = x
-      this.y = y
-
-      this.dest = {
-        x: 0,
-        y: 0
-      }
-
-      this.width = 100
-      this.height = 100
-      this.velocity = 12
-      this.angularVelocity = 7
-      this.rotation = 0
-      this.ctx = ctx
-
-      this.image = playerImage
-    }
-
+    constructor(x, y, w, h, img){
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.img = img;
+}
     draw = () => {
-      this.ctx.translate(this.x, this.y)
-      this.ctx.rotate(this.rotation + 4.7)
-      this.ctx.drawImage(
-        this.image,
-        -this.width / 2, -this.height / 2,
-        this.width, this.height
-      )
-      this.ctx.rotate(-this.rotation - 4.7)
-      this.ctx.translate(-this.x, -this.y)
+        //>>>>>This code gets the coord of the canvas
+        
+        
+        let canvasXY = canvas.getBoundingClientRect();
+        
+
+        let actualMouseX = gMouseX - canvasXY.x;
+        let actualMouseY = gMouseY - canvasXY.y;
+        let centerOfShipX = this.x + 50;
+        let centerOfShipY = this.y + 50;
+    
+        gShipAngleInRads = Math.atan2(
+          actualMouseY - centerOfShipY,
+          actualMouseX - centerOfShipX
+        );
+    
+        ctx.translate(centerOfShipX, centerOfShipY);
+        ctx.rotate(gShipAngleInRads + (90 * Math.PI) / 180);
+        ctx.translate(-centerOfShipX, -centerOfShipY);
+        ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+    
+        ctx.beginPath();
+        ctx.moveTo(centerOfShipX, centerOfShipY);
+        ctx.lineTo(actualMouseX, actualMouseY);
+        ctx.lineWidth = 5;
+        ctx.stroke();
     }
+
+    
 }
 
-let player = new Player(ctx, canvas.width /2, 200);
+const player = new Player(canvas.width/2, 250, 100, 100, player_image)
 
 const background = {
     x: 0,
