244cc57d122ec69c4b57862b55d164a9cb9fe33a
Buckley212 <rob36611@gmail.com>
2021-04-12 15:43:48 -0400

    added zombies

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,9 @@ backgroundImage.src = './resources/assets/gameMap.PNG'
 const player_image = new Image();
 player_image.src = './resources/assets/survivor-idle_shotgun_0.png'
 
+const zombie_image = new Image();
+zombie_image.src = './resources/assets/zombie.png'
+
 let gMouseX = 0;
 let gMouseY = 0;
 let gShipAngleInRads = 0;
@@ -19,22 +22,18 @@ let actualMouseY = 0;
 document.addEventListener("mousemove", (e) => {
     gMouseX = e.clientX;
     gMouseY = e.clientY;
-  });
+});
 
 class Player {
     constructor(x, y, w, h, img){
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-    this.img = img;
-}
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.img = img;
+    }
     draw = () => {
-        //>>>>>This code gets the coord of the canvas
-        
-        
         let canvasXY = canvas.getBoundingClientRect();
-        
 
         let actualMouseX = gMouseX - canvasXY.x;
         let actualMouseY = gMouseY - canvasXY.y;
@@ -58,11 +57,46 @@ class Player {
         ctx.lineWidth = 5;
         ctx.stroke();
     }
-
-    
 }
 
-const player = new Player(canvas.width/2, 250, 100, 100, player_image)
+class Zombie {
+    constructor(health, x, y, w, h, img){
+        this.health = health;
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.img = img;
+    }
+    draw = () => {
+        ctx.drawImage(this.img, this.x, this.y, this.w, this.h)
+    }
+    move = () => {
+        if (this.y > 300){
+            this.y -=1
+        }
+        else { this.y -= 0}
+    }
+}
+
+let zombies = [];
+
+setInterval(function () {
+    zombies.push(new Zombie(100, Math.floor(Math.random()*canvas.width), canvas.height, 100, 100, zombie_image))
+}, 3000)
+
+function detectCollision(rect1, rect2) {
+    if (
+      rect1.x < rect2.x + rect2.w &&
+      rect1.x + rect1.w > rect2.x &&
+      rect1.y < rect2.y + rect2.h &&
+      rect1.y + rect1.h > rect2.y
+    ) {
+        zombie.health -= 50
+    }
+}
+
+const player = new Player(canvas.width/2 - 20, 200, 100, 100, player_image)
 
 const background = {
     x: 0,
@@ -81,6 +115,10 @@ function animate() {
     ctx.clearRect(0,0,canvas.width,canvas.height)
     background.draw();
     player.draw();
+    zombies.forEach(badguy => {
+        badguy.draw()
+        badguy.move();
+    })
 }
 
 let startBtn = document.getElementById('start-btn');
@@ -89,31 +127,3 @@ startBtn.addEventListener('click', function () {
     startScreen.classList.add('load-fade')
     animate()
 })
-
-// draw() {
-//     //>>>>>This code gets the coord of the canvas
-//     let canvasXY = canvas.getBoundingClientRect();
-
-//     let actualMouseX = gMouseX - canvasXY.x;
-//     let actualMouseY = gMouseY - canvasXY.y;
-//     let centerOfShipX = this.x + 52;
-//     let centerOfShipY = this.y + 70;
-
-//     gShipAngleInRads = Math.atan2(
-//       actualMouseY - centerOfShipY,
-//       actualMouseX - centerOfShipX
-//     );
-
-//     context.translate(centerOfShipX, centerOfShipY);
-//     context.rotate(gShipAngleInRads + (90 * Math.PI) / 180);
-//     context.translate(-centerOfShipX, -centerOfShipY);
-//     context.drawImage(this.img, this.x, this.y, this.w, this.h);
-//     context.setTransform(1, 0, 0, 1, 0, 0);
-
-//     // //>>>>>> Draw line from ship to mouse for clarification of points while coding
-//     // context.beginPath();
-//     // context.moveTo(centerOfShipX, centerOfShipY);
-//     // context.lineTo(actualMouseX, actualMouseY);
-//     // context.lineWidth = 5;
-//     // context.stroke();
-//   }
